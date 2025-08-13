3a87d10d23f36f9732cd674c94bbf58108b2f960
manamand2020 <68087238+manamand2020@users.noreply.github.com>
2025-08-13 13:10:37 -0700

    Added proper check for mux nbrs for route delete (#127)
    tunnelroutes were not getting removed and route checker was complaining about some unaccounted routes.
    Added proper check for mux neighbors to avoid tunnel route deletion.
    
    Signed-off-by: Manas Kumar Mandal <manamand@cisco.com>

--- orchagent/muxorch.cpp
+++ orchagent/muxorch.cpp
@@ -2013,7 +2013,9 @@ void MuxOrch::removeStandaloneTunnelRoute(IpAddress neighborIp)
     NeighborEntry neighbor;
     MacAddress mac;
 
-    if(!gNeighOrch->getNeighborEntry(neighborIp, neighbor, mac))
+    // remove the route prefix if its not a mux neighbor
+    if (!gNeighOrch->getNeighborEntry(neighborIp, neighbor, mac) ||
+            !isMuxPortNeighbor(neighborIp, mac, neighbor.alias))
     {
         IpPrefix pfx = neighborIp.to_string();
         remove_route(pfx);
