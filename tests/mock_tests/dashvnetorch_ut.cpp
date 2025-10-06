sonic-net/sonic-swss

add CRM checks to DashVnetOrch UTs
Signed-off-by: Lawrence Lee <lawlee@microsoft.com>

tests/mock_tests/dashvnetorch_ut.cpp
@@ -27,17 +27,42 @@ namespace dashvnetorch_test
     using ::testing::Return;
     using ::testing::Throw;
     using ::testing::DoAll;
+    using ::testing::DoDefault;
     using ::testing::SetArrayArgument;
     using ::testing::SetArgPointee;
+    using ::testing::InSequence;
 
     class DashVnetOrchTest : public MockDashOrchTest
     {
     protected:
-        int GetCrmUsedCount(CrmResourceType type)
+        uint32_t GetCrmUsedCount(CrmResourceType type)
         {
-            CrmOrch::CrmResourceEntry entry = CrmOrch::CrmResourceEntry("", CrmThresholdType::CRM_PERCENTAGE, 0, 1);
-            gCrmOrch->getResAvailability(type, entry);
-            return entry.countersMap["STATS"].usedCounter;
+            uint32_t count;
+            if (gCrmOrch->getCrmResUsedCounter(type, count))
+            {
+                return count;
+            }
+            return 0;
+        }
+
+        std::pair<uint32_t, uint32_t> GetVnetMapCrm()
+        {
+            uint32_t used_ca_to_pa = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_CA_TO_PA);
+            uint32_t used_pa_validation = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_PA_VALIDATION);
+            return std::make_pair(used_ca_to_pa, used_pa_validation);
+        }
+
+        bool CheckExpectedCrmMatch(uint32_t expected_ca_to_pa, uint32_t expected_pa_validation)
+        {
+            uint32_t actual_ca_to_pa, actual_pa_validation;
+            std::tie(actual_ca_to_pa, actual_pa_validation) = GetVnetMapCrm();
+            bool matches = actual_ca_to_pa == expected_ca_to_pa && actual_pa_validation == expected_pa_validation;
+            if (!matches)
+            {
+                std::cout << "Expected CA to PA: " << expected_ca_to_pa << ", Actual CA to PA: " << actual_ca_to_pa << std::endl;
+                std::cout << "Expected PA Validation: " << expected_pa_validation << ", Actual PA Validation: " << actual_pa_validation << std::endl;
+            }
+            return matches;
         }
 
         void ApplySaiMock() override
@@ -51,6 +76,7 @@ namespace dashvnetorch_test
         void PostSetUp() override
         {
             CreateApplianceEntry();
+            std::tie(orig_ca_to_pa, orig_pa_validation) = GetVnetMapCrm();
         }
         void PreTearDown() override
         {
@@ -60,93 +86,98 @@ namespace dashvnetorch_test
             DEINIT_SAI_API_MOCK(dash_vnet);
         }
 
+        int orig_ca_to_pa, orig_pa_validation, expected_ca_to_pa, expected_pa_validation;
     };
 
     TEST_F(DashVnetOrchTest, AddRemoveVnet)
     {
-        std::vector<sai_status_t> exp_status = {SAI_STATUS_SUCCESS};
+        expected_ca_to_pa = orig_ca_to_pa + 1;
+        expected_pa_validation = orig_pa_validation + 1;
+
         AddRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
-        EXPECT_CALL(*mock_sai_dash_vnet_api, create_vnets)
-            .Times(1).WillOnce(DoAll(
-                SetArgPointee<5>(0x1234),
-                SetArrayArgument<6>(exp_status.begin(), exp_status.end()),
-                Return(SAI_STATUS_SUCCESS)
-            )
-            );
+
+        {
+            InSequence seq;
+            EXPECT_CALL(*mock_sai_dash_vnet_api, create_vnets).Times(1);
+            EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, create_outbound_ca_to_pa_entries).Times(1);
+            EXPECT_CALL(*mock_sai_dash_pa_validation_api, create_pa_validation_entries).Times(1);
+            EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, remove_outbound_ca_to_pa_entries).Times(1);
+            EXPECT_CALL(*mock_sai_dash_pa_validation_api, remove_pa_validation_entries).Times(1);
+            EXPECT_CALL(*mock_sai_dash_vnet_api, remove_vnets).Times(1);
+        }
         CreateVnet();
-        EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, create_outbound_ca_to_pa_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<5>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
-        EXPECT_CALL(*mock_sai_dash_pa_validation_api, create_pa_validation_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<5>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
         AddVnetMap();
 
-        EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, remove_outbound_ca_to_pa_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<3>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
+
         RemoveVnetMap();
-        EXPECT_CALL(*mock_sai_dash_pa_validation_api, remove_pa_validation_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<3>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
-        EXPECT_CALL(*mock_sai_dash_vnet_api, remove_vnets)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<3>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
+
+        // Removing all VNET maps using a specific PA validation entry should also result in the removal
+        // of the PA validation entry, even if the VNET still exists
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
+
         RemoveVnet();
+
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, AddVnetMapMissingVnetFails)
     {
+        AddRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
         EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, create_outbound_ca_to_pa_entries)
             .Times(0);
         EXPECT_CALL(*mock_sai_dash_pa_validation_api, create_pa_validation_entries)
             .Times(0);
-        AddRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
         AddVnetMap(false);
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, AddExistingOutboundCaToPaSuccessful)
     {
-        AddRoutingType(dash::route_type::ENCAP_TYPE_VXLAN); 
+        AddRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
         CreateVnet();
-        AddVnetMap();
-        std::vector<sai_status_t> exp_status = {SAI_STATUS_ITEM_ALREADY_EXISTS};
 
-        int expectedUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_CA_TO_PA);
         EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, create_outbound_ca_to_pa_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<5>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
-        AddVnetMap(); 
-        int actualUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_CA_TO_PA);
-        EXPECT_EQ(expectedUsed, actualUsed);
+            .Times(2);
+        AddVnetMap();
+        std::tie(expected_ca_to_pa, expected_pa_validation) = GetVnetMapCrm();
+        AddVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, RemoveNonexistVnetMapFails)
     {
-        int expectedUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_CA_TO_PA);
-        std::vector<sai_status_t> exp_status = {SAI_STATUS_ITEM_NOT_FOUND};
+        CreateVnet();
         EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, remove_outbound_ca_to_pa_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<3>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
-        RemoveVnetMap(); 
-        int actualUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_CA_TO_PA);
-        EXPECT_EQ(expectedUsed, actualUsed);
+            .Times(1);
+        RemoveVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, InvalidEncapVnetMapFails)
     {
         AddRoutingType(dash::route_type::ENCAP_TYPE_UNSPECIFIED);
         CreateVnet();
-        AddVnetMap();
         EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, create_outbound_ca_to_pa_entries)
             .Times(0);
         AddVnetMap();
+        AddVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, AddExistPaValidationSuccessful)
     {
         AddRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
         CreateVnet();
-        std::vector<sai_status_t> exp_status = {SAI_STATUS_ITEM_ALREADY_EXISTS};
-        int expectedUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_PA_VALIDATION);
+
+        // First add operation will propogate to SAI normally
+        // Second add operation will not propogate to SAI since orchagent caches PA validation entries and can check for duplicates
         EXPECT_CALL(*mock_sai_dash_pa_validation_api, create_pa_validation_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<5>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
+            .Times(1);
         AddVnetMap();
-        int actualUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_PA_VALIDATION);
-        EXPECT_EQ(expectedUsed, actualUsed);
+        std::tie(expected_ca_to_pa, expected_pa_validation) = GetVnetMapCrm();
+        AddVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, RemovePaValidationInUseFails)
@@ -155,14 +186,42 @@ namespace dashvnetorch_test
         CreateVnet();
         AddVnetMap();
 
-        int expectedUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_PA_VALIDATION);
-        std::vector<sai_status_t> exp_status = {SAI_STATUS_OBJECT_IN_USE};
+        std::vector<sai_status_t> exp_status = { SAI_STATUS_OBJECT_IN_USE };
 
         EXPECT_CALL(*mock_sai_dash_pa_validation_api, remove_pa_validation_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<3>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
+            .Times(1)
+            .WillOnce(DoAll(SetArrayArgument<3>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
+        std::tie(expected_ca_to_pa, expected_pa_validation) = GetVnetMapCrm();
         RemoveVnet(false);
 
-        int actualUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_PA_VALIDATION);
-        EXPECT_EQ(expectedUsed, actualUsed);
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
+    }
+
+    TEST_F(DashVnetOrchTest, AddRemoveMultipleVnetMapsWithSameUnderlayIp)
+    {
+        AddRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
+        CreateVnet();
+        EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, create_outbound_ca_to_pa_entries).Times(2);
+        EXPECT_CALL(*mock_sai_dash_pa_validation_api, create_pa_validation_entries).Times(1); 
+        EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, remove_outbound_ca_to_pa_entries).Times(2);
+        EXPECT_CALL(*mock_sai_dash_pa_validation_api, remove_pa_validation_entries).Times(1); 
+
+        expected_ca_to_pa = orig_ca_to_pa + 1;
+        expected_pa_validation = orig_pa_validation + 1;
+        AddVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
+
+        expected_ca_to_pa++;
+        AddVnetMap(true, vnet_map_ip2);
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
+
+        expected_ca_to_pa--;
+        RemoveVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
+
+        expected_ca_to_pa--;
+        expected_pa_validation--;
+        RemoveVnetMap(true, vnet_map_ip2);
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
     }
 }

Azure/sonic-swss.msft

tests/mock_tests/dashvnetorch_ut.cpp
@@ -27,6 +27,7 @@ namespace dashvnetorch_test
     using ::testing::Return;
     using ::testing::Throw;
     using ::testing::DoAll;
+    using ::testing::DoDefault;
     using ::testing::SetArrayArgument;
     using ::testing::SetArgPointee;
     using ::testing::InSequence;
@@ -34,11 +35,34 @@ namespace dashvnetorch_test
     class DashVnetOrchTest : public MockDashOrchTest
     {
     protected:
-        int GetCrmUsedCount(CrmResourceType type)
+        uint32_t GetCrmUsedCount(CrmResourceType type)
         {
-            CrmOrch::CrmResourceEntry entry = CrmOrch::CrmResourceEntry("", CrmThresholdType::CRM_PERCENTAGE, 0, 1);
-            gCrmOrch->getResAvailability(type, entry);
-            return entry.countersMap["STATS"].usedCounter;
+            uint32_t count;
+            if (gCrmOrch->getCrmResUsedCounter(type, count))
+            {
+                return count;
+            }
+            return 0;
+        }
+
+        std::pair<uint32_t, uint32_t> GetVnetMapCrm()
+        {
+            uint32_t used_ca_to_pa = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_CA_TO_PA);
+            uint32_t used_pa_validation = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_PA_VALIDATION);
+            return std::make_pair(used_ca_to_pa, used_pa_validation);
+        }
+
+        bool CheckExpectedCrmMatch(uint32_t expected_ca_to_pa, uint32_t expected_pa_validation)
+        {
+            uint32_t actual_ca_to_pa, actual_pa_validation;
+            std::tie(actual_ca_to_pa, actual_pa_validation) = GetVnetMapCrm();
+            bool matches = actual_ca_to_pa == expected_ca_to_pa && actual_pa_validation == expected_pa_validation;
+            if (!matches)
+            {
+                std::cout << "Expected CA to PA: " << expected_ca_to_pa << ", Actual CA to PA: " << actual_ca_to_pa << std::endl;
+                std::cout << "Expected PA Validation: " << expected_pa_validation << ", Actual PA Validation: " << actual_pa_validation << std::endl;
+            }
+            return matches;
         }
 
         void ApplySaiMock() override
@@ -52,6 +76,7 @@ namespace dashvnetorch_test
         void PostSetUp() override
         {
             CreateApplianceEntry();
+            std::tie(orig_ca_to_pa, orig_pa_validation) = GetVnetMapCrm();
         }
         void PreTearDown() override
         {
@@ -61,13 +86,17 @@ namespace dashvnetorch_test
             DEINIT_SAI_API_MOCK(dash_vnet);
         }
 
+        int orig_ca_to_pa, orig_pa_validation, expected_ca_to_pa, expected_pa_validation;
     };
 
     TEST_F(DashVnetOrchTest, AddRemoveVnet)
     {
-        std::vector<sai_status_t> exp_status = {SAI_STATUS_SUCCESS};
+        expected_ca_to_pa = orig_ca_to_pa + 1;
+        expected_pa_validation = orig_pa_validation + 1;
+
         AddVnetEncapRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
         AddPLRoutingType();
+
         {
             InSequence seq;
             EXPECT_CALL(*mock_sai_dash_vnet_api, create_vnets).Times(1);
@@ -84,68 +113,74 @@ namespace dashvnetorch_test
         AddPortMap();
         AddVnetMapPL();
 
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
+
         RemoveVnetMap();
         RemoveVnetMapPL();
+
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
         RemoveVnet();
+
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, AddVnetMapMissingVnetFails)
     {
+        AddVnetEncapRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
         EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, create_outbound_ca_to_pa_entries)
             .Times(0);
         EXPECT_CALL(*mock_sai_dash_pa_validation_api, create_pa_validation_entries)
             .Times(0);
-        AddVnetEncapRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
         AddVnetMap(false);
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, AddExistingOutboundCaToPaSuccessful)
     {
         AddVnetEncapRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
         CreateVnet();
-        AddVnetMap();
-        std::vector<sai_status_t> exp_status = {SAI_STATUS_ITEM_ALREADY_EXISTS};
 
-        int expectedUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_CA_TO_PA);
         EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, create_outbound_ca_to_pa_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<5>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
-        AddVnetMap(); 
-        int actualUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_CA_TO_PA);
-        EXPECT_EQ(expectedUsed, actualUsed);
+            .Times(2);
+        AddVnetMap();
+        std::tie(expected_ca_to_pa, expected_pa_validation) = GetVnetMapCrm();
+        AddVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, RemoveNonexistVnetMapFails)
     {
-        int expectedUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_CA_TO_PA);
-        std::vector<sai_status_t> exp_status = {SAI_STATUS_ITEM_NOT_FOUND};
+        CreateVnet();
         EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, remove_outbound_ca_to_pa_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<3>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
-        RemoveVnetMap(); 
-        int actualUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_CA_TO_PA);
-        EXPECT_EQ(expectedUsed, actualUsed);
+            .Times(1);
+        RemoveVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, InvalidEncapVnetMapFails)
     {
         AddVnetEncapRoutingType(dash::route_type::ENCAP_TYPE_UNSPECIFIED);
         CreateVnet();
-        AddVnetMap();
         EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, create_outbound_ca_to_pa_entries)
             .Times(0);
         AddVnetMap();
+        AddVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, AddExistPaValidationSuccessful)
     {
         AddVnetEncapRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
         CreateVnet();
-        std::vector<sai_status_t> exp_status = {SAI_STATUS_ITEM_ALREADY_EXISTS};
-        int expectedUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_PA_VALIDATION);
+
+        // First add operation will propogate to SAI normally
+        // Second add operation will not propogate to SAI since orchagent caches PA validation entries and can check for duplicates
         EXPECT_CALL(*mock_sai_dash_pa_validation_api, create_pa_validation_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<5>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
+            .Times(1);
         AddVnetMap();
-        int actualUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_PA_VALIDATION);
-        EXPECT_EQ(expectedUsed, actualUsed);
+        std::tie(expected_ca_to_pa, expected_pa_validation) = GetVnetMapCrm();
+        AddVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
     }
 
     TEST_F(DashVnetOrchTest, RemovePaValidationInUseFails)
@@ -154,14 +189,42 @@ namespace dashvnetorch_test
         CreateVnet();
         AddVnetMap();
 
-        int expectedUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_PA_VALIDATION);
-        std::vector<sai_status_t> exp_status = {SAI_STATUS_OBJECT_IN_USE};
+        std::vector<sai_status_t> exp_status = { SAI_STATUS_OBJECT_IN_USE };
 
         EXPECT_CALL(*mock_sai_dash_pa_validation_api, remove_pa_validation_entries)
-            .Times(1).WillOnce(DoAll(SetArrayArgument<3>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
+            .Times(1)
+            .WillOnce(DoAll(SetArrayArgument<3>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
+        std::tie(expected_ca_to_pa, expected_pa_validation) = GetVnetMapCrm();
         RemoveVnet(false);
 
-        int actualUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_PA_VALIDATION);
-        EXPECT_EQ(expectedUsed, actualUsed);
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
+    }
+
+    TEST_F(DashVnetOrchTest, AddRemoveMultipleVnetMapsWithSameUnderlayIp)
+    {
+        AddVnetEncapRoutingType(dash::route_type::ENCAP_TYPE_VXLAN);
+        CreateVnet();
+        EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, create_outbound_ca_to_pa_entries).Times(2);
+        EXPECT_CALL(*mock_sai_dash_pa_validation_api, create_pa_validation_entries).Times(1); 
+        EXPECT_CALL(*mock_sai_dash_outbound_ca_to_pa_api, remove_outbound_ca_to_pa_entries).Times(2);
+        EXPECT_CALL(*mock_sai_dash_pa_validation_api, remove_pa_validation_entries).Times(1); 
+
+        expected_ca_to_pa = orig_ca_to_pa + 1;
+        expected_pa_validation = orig_pa_validation + 1;
+        AddVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
+
+        expected_ca_to_pa++;
+        AddVnetMap(true, vnet_map_ip2);
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
+
+        expected_ca_to_pa--;
+        RemoveVnetMap();
+        EXPECT_TRUE(CheckExpectedCrmMatch(expected_ca_to_pa, expected_pa_validation));
+
+        expected_ca_to_pa--;
+        expected_pa_validation--;
+        RemoveVnetMap(true, vnet_map_ip2);
+        EXPECT_TRUE(CheckExpectedCrmMatch(orig_ca_to_pa, orig_pa_validation));
     }
 }