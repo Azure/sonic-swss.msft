e5176effffa43587057e18125a82ac6f69867ddc
saksarav <sakthivadivu.saravanaraj@nokia.com>
2026-07-30 15:16:25 -0400

    Cherry-pick PR 3976 -  Populate the Voq system Port information for the local port when the Port is removed and created when the Speed is changed dynamically via GCU
    Signed-off-by: saksarav <sakthivadivu.saravanaraj@nokia.com>

--- orchagent/portsorch.h
+++ orchagent/portsorch.h
@@ -119,6 +119,13 @@ struct queueInfo
     sai_uint8_t index;
 };
 
+struct systemPortMapInfo
+{
+    sai_object_id_t system_port_id;
+    SystemPortInfo system_port_info;
+    bool info_valid = false;
+};
+
 template<typename T>
 struct PortCapability
 {
@@ -297,6 +304,7 @@ private:
     unique_ptr<Table> m_portSerdesIdToPortIdTable;
     unique_ptr<Table> m_portTable;
     unique_ptr<Table> m_sendToIngressPortTable;
+    unique_ptr<Table> m_systemPortTable;
     unique_ptr<Table> m_gearboxTable;
     unique_ptr<CounterNameMapUpdater> m_queueCounterNameMapUpdater;
     unique_ptr<Table> m_voqTable;
@@ -587,10 +595,11 @@ private:
     map<string, Port::Role> m_recircPortRole;
 
     //map key is tuple of <attached_switch_id, core_index, core_port_index>
-    map<tuple<int, int, int>, sai_object_id_t> m_systemPortOidMap;
+    map<tuple<int, int, int>, systemPortMapInfo> m_systemPortOidMap;
     sai_uint32_t m_systemPortCount;
     bool getSystemPorts();
     bool addSystemPorts();
+    void updateSystemPort(Port &port);
     unique_ptr<Table> m_tableVoqSystemLagTable;
     unique_ptr<Table> m_tableVoqSystemLagMemberTable;
     void voqSyncAddLag(Port &lag);
